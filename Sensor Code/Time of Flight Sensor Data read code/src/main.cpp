15ac2cf1b13966a78b3546041d7f864d657e76e3
Gabe <gwurtsbaugh@student.cccs.edu>
2024-01-04 12:26:06 -0700

    Distances per zone and corisponding zone

--- Sensor Code/Time of Flight Sensor Data read code/src/main.cpp
+++ Sensor Code/Time of Flight Sensor Data read code/src/main.cpp
@@ -1,10 +1,17 @@
+/*
+This is the adapted code from the documentation that outputs and arrary of distances in mm.
+
+I wasn't sure how to read or get values from the array it was producing so I went ahead and wrote somecode to make a "Monitoring Array" so that we know where our data zone on the sensor are.
+
+While they match the order in the documentation they are reflected on both the y and x axis. This allows us to unmirror the image that the sensor is seeing / the zones in the documentation.
+*/
+
 #include <Arduino.h>
 #include <SparkFun_VL53L5CX_Library.h>
 #include <Wire.h>
 
-
 SparkFun_VL53L5CX myImager;
-VL53L5CX_ResultsData measurementData; // Result data class structure, 1356 byes of RAM
+VL53L5CX_ResultsData measurementData; // Result data class structure, 1356 bytes of RAM
 
 int imageResolution = 0; //Used to pretty print output
 int imageWidth = 0; //Used to pretty print output
@@ -30,11 +37,13 @@ void setup()
   imageResolution = myImager.getResolution(); //Query sensor for current resolution - either 4x4 or 8x8
   imageWidth = sqrt(imageResolution); //Calculate printing width
 
+  //The sensor starts collecting data
   myImager.startRanging();
 }
 
 void loop()
 {
+  //Process to write an array into Serial Monitor so that you can see what the sensor is seeing--------------------------------------------------
   //Poll sensor for new data
   if (myImager.isDataReady() == true)
   {
@@ -44,6 +53,7 @@ void loop()
       //Pretty-print data with increasing y, decreasing x to reflect reality
       for (int y = 0 ; y <= imageWidth * (imageWidth - 1) ; y += imageWidth)
       {
+        
         for (int x = imageWidth - 1 ; x >= 0 ; x--)
         {
           Serial.print("\t");
@@ -54,6 +64,35 @@ void loop()
       Serial.println();
     }
   }
+
+/**/
+      //So that I can understand what the call values in the array look like
+      Serial.println("Monitoring Array");
+      for (int y = 0 ; y <= imageWidth * (imageWidth - 1) ; y += imageWidth)
+      {
+        //Start new row
+        Serial.print("Y Val: ");
+        Serial.println(y);
+        Serial.println();
+
+        for (int x = imageWidth - 1 ; x >= 0 ; x--)
+        {
+          Serial.print("\t");
+          Serial.print(x+y);
+        }
+        Serial.println();
+      }
+      Serial.println();
+    
+  
+  /**/
+  //End of Array Writing Process
+
+  
+
+
+
+  
 
   delay(5); //Small delay between polling
 }
