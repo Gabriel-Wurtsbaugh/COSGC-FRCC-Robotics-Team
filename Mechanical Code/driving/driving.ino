a1f79b924f14d70bb6af5eddec9053a64059b3d4
dennarahl <154643817+dennarahl@users.noreply.github.com>
2024-01-28 15:05:28 -0700

    Create driving.ino

---
+++ Mechanical Code/driving/driving.ino
@@ -0,0 +1,27 @@
+#include "Freenove_4WD_Car_For_ESP32.h"
+
+void setup()
+{
+  PCA9685_Setup();       //Initializes the chip that controls the motor
+}
+
+void loop()
+{
+  Motor_Move(2000, 2000, 2000, 2000);     //go forward
+  delay(1000);
+  Motor_Move(0, 0, 0, 0);                 //stop
+  delay(1000);
+  Motor_Move(-2000, -2000, -2000, -2000); //go back
+  delay(1000);
+  Motor_Move(0, 0, 0, 0);                 //stop
+  delay(1000);
+
+  Motor_Move(-2000, -2000, 2000, 2000);   //turn left
+  delay(1000);
+  Motor_Move(0, 0, 0, 0);                 //stop
+  delay(1000);
+  Motor_Move(2000, 2000, -2000, -2000);   //turn right
+  delay(1000);
+  Motor_Move(0, 0, 0, 0);                 //stop
+  delay(1000);
+}
