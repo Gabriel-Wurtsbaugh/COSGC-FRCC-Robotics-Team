eb715ec4595e9dced5db039647dbe9c30cfd7ca5
Gabe <gwurtsbaugh@student.cccs.edu>
2024-01-28 13:11:10 -0700

    More progress

--- Sensor Code/Object Recognition/src/main.cpp
+++ Sensor Code/Object Recognition/src/main.cpp
@@ -7,14 +7,20 @@
 SparkFun_VL53L5CX myImager;
 VL53L5CX_ResultsData measurementData; // Result data class structure, 1356 bytes of RAM, Used to store distances
 
+
+//Universal Variables
 int imageResolution = 0; //Used to pretty print output
 int imageWidth = 0; //Used to pretty print output
-int minDistance = 300;//Set nearest distance an object can get to the sensor (mm)
+int minDistance = 300; //Set nearest distance an object can get to the sensor (mm)
+int height1 = 100; //Know height of the ToF sensor off the ground on a flat plane (mm)
+
 
 // put function declarations here:
 void sensorIntitiation();
 bool dataProcessed();
-int objectDetection();
+int objectDetection(int* smol, bool* detection);
+int heightCalculator(int smol);
+
 
 void setup() {
   // put your setup code here, to run once:
@@ -22,28 +28,32 @@ void setup() {
   sensorIntitiation();
 }
 
+//------------------------------------------------------------------------------------------------------------------------------------------------------
 void loop() {
   // put your main code here, to run repeatedly:
   delay(1000);
 
+
+  //Variables
+  int smol;
+  bool detection;
+
+
   //Detect if the sensor has data that can be collected and checks the data for any objects within range. 
   if (dataProcessed() == true) 
   {
-    if (objectDetection() == true)
+    //Runs objectDetection algorithm
+    objectDetection(&smol, &detection);
+
+    if (detection == true)
     {
-      {
-        
-
-
-
-
-
-
-      }
+      heightCalculator(smol);
     }
   }
 
 }
+//------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 
 // put function definitions here:
 //Function to intitiat ToF Sensor Settings so I can Clean my code a bit and make it easier to copy and paste
@@ -82,11 +92,12 @@ bool dataProcessed() {
   return false;
 }
 
+
 //Checks to see if an object has come within the min detect distance
-int objectDetection() {
+int objectDetection(int* smol, bool* detection) {
   //Variables
-  bool detection = false;
-  int x = 0; //Object as tall or taller than rover if x=1, shorter if x=0
+  *detection = false;
+  *smol = 0; //Object as tall or taller than rover if x=1, shorter if x=0
 
 
   for (int y = 0 ; y <= imageWidth * (imageWidth - 1) ; y += imageWidth)
@@ -95,11 +106,11 @@ int objectDetection() {
     {
       if (measurementData.distance_mm[x+y] < minDistance)
       {
-        detection = true; //returns true if there is an object within minimum distance.
-      } else {return false;} //quits out the function if no object is detected
+        *detection = true; //returns true if there is an object within minimum distance.
+      } else {return 0;} //quits out the function if no object is detected
     }
 
-    if (detection == true)
+    if (*detection == true)
     {
       //Variables 
       int array[4] = {(35,36,27,28)}; 
@@ -117,9 +128,44 @@ int objectDetection() {
       //Checks to see if  the center of the sensor can see the object
       if (ave <= minDistance)
       {
-        x = 1;
+        *smol = 1;
       }
-    }
+    }else {return 0;}
   }
-  return detection, x;
+  return 0;
+} 
+
+int heightCalculator(int smol) {
+  //All of the variable nesscary for height calculations
+  //All values in mm
+  //Distance Variables
+  int distance1 = 0;
+  int distance2 = 0;
+  int distance3 = 0;
+
+  //Height Variables
+  int hieght2 = 0;
+  int height3 = 0;
+  int heightTotal = 0;
+
+  //Angle Variables
+  int theta1 = 0;
+  int theta2 = 0;
+  
+  if (smol == 1)
+  {
+      //Measures D1
+      int array[4] = {(35,36,27,28)}; 
+      
+      //Sums the values for the distances read by the center four zones on the sensor.
+      for (int i = 0; i <= 3; ++i)
+      {
+        distance1 = distance1 + measurementData.distance_mm[array[i]];
+      }
+
+      theta1 = atan (height1/distance1); //Calculates theta 1
+
+      
+
+  }
 }
