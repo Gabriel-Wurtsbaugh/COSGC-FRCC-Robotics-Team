9fe29b9c830f18be91e0473d9ebef8c026cc460e
Gabe Labtop <gwurtsy@gmail.com>
2024-02-25 14:26:58 -0700

    arg

--- Mechanical Code/servo/Servo Testing/src/main.cpp
+++ Mechanical Code/servo/Servo Testing/src/main.cpp
@@ -3,36 +3,31 @@
 
 
 Servo myservo;
+Servo myservo2;
+
 
 int position = 0;
-
-
+int ServoPin = 4;
+int ServoPin2 = 5;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
   myservo.setPeriodHertz(50);
-  myservo.attach(5,500,2400);
+  myservo.attach(ServoPin,500,2400);
+  myservo2.setPeriodHertz(50);  
+  myservo2.attach(ServoPin2,500,2400);
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  delay(3000);
-
-  position = 0;
-  myservo.write(position);
-  Serial.println(myservo.read());
-
-  delay(3000);
+  
 
   position = 90;
   myservo.write(position);
   Serial.println(myservo.read());
 
-  delay(3000);
-
-  position = 180;
-  myservo.write(position);
-  Serial.println(myservo.read());
+  myservo2.write(position);
 }
 
