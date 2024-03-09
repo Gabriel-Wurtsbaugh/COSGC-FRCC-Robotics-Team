81d6e986f9f111df4a9f49660a4f7ca861640b3c
Shannon <shannonmvoth@gmail.com>
2024-03-09 14:41:10 -0700

    ultrasonic stub working

---
+++ Sensor Code/Ultrasonic/main.cpp
@@ -0,0 +1,36 @@
+
+#include <Arduino.h>
+#include <UltrasonicSensor.h>
+#include <NewPing.h>
+
+#define TRIGGER_PIN 13
+#define ECHO_PIN 14
+#define MAX_DISTANCE 200
+
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
+
+void setup()
+{
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  delay(2000);
+  unsigned int uS = sonar.ping();
+  if (uS == 0)
+  {
+    Serial.println("ping out of max range");
+  }
+  else
+  {
+    Serial.print("uS: ");
+    Serial.println(uS);
+    pinMode(ECHO_PIN, OUTPUT);
+    digitalWrite(ECHO_PIN, LOW);
+    pinMode(ECHO_PIN, INPUT);
+    Serial.print("Ping: ");
+    Serial.print(uS / US_ROUNDTRIP_CM); // US_ROUNDTRIP_CM def in library as 57 (microsec takes to travel 1cm)
+    Serial.println("cm");
+  }
+}
