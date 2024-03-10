f1c0117ce9f36a2a641a94f235c8566d6d59e8ad
Gabe Labtop <gwurtsy@gmail.com>
2024-03-10 14:48:48 -0600

    Squashed commit of the following:
    commit 7823e0d9fa3485dd2ca819cc0efc29245601800b
    Author: Gabe Labtop <gwurtsy@gmail.com>
    Date:   Sun Mar 10 14:46:13 2024 -0600
    
        asdf
    
    commit 257494eee4d5089d437111d3e227d73b0ba66272
    Author: Gabe Labtop <gwurtsy@gmail.com>
    Date:   Sun Mar 3 15:39:37 2024 -0700
    
        bugz
    
    commit 3c5c9ca3eb45b3538008a330e4be372a338b06dc
    Author: Gabe Labtop <gwurtsy@gmail.com>
    Date:   Sun Feb 25 14:29:50 2024 -0700
    
        l;kkj
    
    commit e51dda50606d9ee92c329e61b815fb187113deee
    Author: Gabe <gwurtsbaugh@student.cccs.edu>
    Date:   Sat Feb 24 20:52:16 2024 -0700
    
        Continued bug patching. The bugs have stolen my lunch money and left me bruised and  broke on the side of the highway
    
    commit 652b2b88263a2dde1efb753b7a767737933fbed3
    Author: Gabe <gwurtsbaugh@student.cccs.edu>
    Date:   Sat Feb 24 20:22:40 2024 -0700
    
        .
    
    commit 17a219b2f232ef8482342b04b465c0a6126929ed
    Author: Gabe <gwurtsbaugh@student.cccs.edu>
    Date:   Sun Feb 11 14:44:46 2024 -0700
    
        It borken, Trying to fix it
    
    commit 1ce795fdda05d4b747acf9b375fd96b7ef721906
    Author: Gabe <gwurtsbaugh@student.cccs.edu>
    Date:   Sun Feb 4 14:36:08 2024 -0700
    
        small object height distance
    
    commit 3504b481a0c4c4bc77d2084b5842e4d0e8a9146c
    Author: Gabe <gwurtsbaugh@student.cccs.edu>
    Date:   Sat Feb 3 17:51:20 2024 -0700
    
        finishes taller object height calculation....I can only calc the height of an object directly in front of the rover
    
    commit 0437b694db4eef401cbeae892bacbf024be1f0b4
    Merge: 51fd4a8 c6d5a72
    Author: Gabe <gwurtsbaugh@student.cccs.edu>
    Date:   Fri Feb 2 14:38:45 2024 -0700
    
        Merge branch 'gabe/Servo-Work'
    
    commit c6d5a728eacbcf565c3038dabc95aa7b15614bc4
    Author: Gabe <gwurtsbaugh@student.cccs.edu>
    Date:   Thu Feb 1 19:42:34 2024 -0700
    
        ?
    
    commit 8f92e51b18b5b9deb5e400ef184b562580b8b99a
    Author: Gabe <gwurtsbaugh@student.cccs.edu>
    Date:   Thu Feb 1 19:42:30 2024 -0700
    
        Adds barebones servo testing code

--- Sensor Code/Object Recognition/src/main.cpp
+++ Sensor Code/Object Recognition/src/main.cpp
@@ -2,17 +2,26 @@
 #include <SparkFun_VL53L5CX_Library.h>
 #include <Wire.h>
 #include <cmath>
+#include <ESP32Servo.h>
+
+
 
 //Data Structures
 SparkFun_VL53L5CX myImager;
 VL53L5CX_ResultsData measurementData; // Result data class structure, 1356 bytes of RAM, Used to store distances
+Servo rotationalServo; //Creates servo objects
+Servo pitchServo;
+
+//ToF Servo Assembly Variables
+int rotServoPin = 4;
+int pitchServoPin = 5;
 
 
 //Universal Variables
 int imageResolution = 0; //Used to pretty print output
 int imageWidth = 0; //Used to pretty print output
-int minDistance = 300; //Set nearest distance an object can get to the sensor (mm)
-int height1 = 100; //Know height of the ToF sensor off the ground on a flat plane (mm)
+int minDistance = 150; //Set nearest distance an object can get to the sensor (mm)
+int height1 =128; //Know height of the ToF sensor off the ground on a flat plane (mm)
 
 
 // put function declarations here:
@@ -20,12 +29,24 @@ void sensorIntitiation();
 bool dataProcessed();
 int objectDetection(int* smol, bool* detection);
 int heightCalculator(int smol);
+int getDistance();
 
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
+
+  //Runs sensor intiation function
   sensorIntitiation();
+
+  //Sets up servo objects to be used in the rest of the code
+  rotationalServo.setPeriodHertz(50);
+  
+  pitchServo.setPeriodHertz(50);
+  rotationalServo.attach(rotServoPin,500,2400);
+  pitchServo.attach(pitchServoPin,500,2400);
+
+  rotationalServo.write(90);
 }
 
 //------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -42,12 +63,20 @@ void loop() {
   //Detect if the sensor has data that can be collected and checks the data for any objects within range. 
   if (dataProcessed() == true) 
   {
+    Serial.println("Data Ready!");
+
+    delay(1000);
+
     //Runs objectDetection algorithm
+    Serial.println("Running Object detection");
     objectDetection(&smol, &detection);
 
     if (detection == true)
     {
-      heightCalculator(smol);
+      int x = heightCalculator(smol);
+
+      Serial.println("Height= ");
+      Serial.println(x);
     }
   }
 
@@ -58,6 +87,15 @@ void loop() {
 // put function definitions here:
 //Function to intitiat ToF Sensor Settings so I can Clean my code a bit and make it easier to copy and paste
 void sensorIntitiation() {
+
+/*
+|
+|
+Intializes the VL53L5CXm Time of flight sensor so that it is prepared to start requesting data from
+|
+|
+*/
+
   delay(1000);
   Serial.println("SparkFun VL53L5CX Imager Example");
 
@@ -82,6 +120,7 @@ void sensorIntitiation() {
 
 //checks to see if data can be collected from sensor and if true reads the data into an array
 bool dataProcessed() {
+//Checks to see if the TOF sensor has data prepared to be outputed
   if (myImager.isDataReady() == true)
   {
     if (myImager.getRangingData(&measurementData)) //Read distance data into array
@@ -95,6 +134,13 @@ bool dataProcessed() {
 
 //Checks to see if an object has come within the min detect distance
 int objectDetection(int* smol, bool* detection) {
+/*
+Detects if an object is within a set minimum distance.
+Please not that the set distance is not a flat plane and is isntead curved
+
+After an object is detected it checks to see if the object is within minimum distance right in front of it
+to check if the object is taller than or shorter than the rover.
+*/
   //Variables
   *detection = false;
   *smol = 0; //Object as tall or taller than rover if x=1, shorter if x=0
@@ -106,7 +152,31 @@ int objectDetection(int* smol, bool* detection) {
     {
       if (measurementData.distance_mm[x+y] < minDistance)
       {
+        Serial.println("Object Detected!");
+
         *detection = true; //returns true if there is an object within minimum distance.
+        
+        //Output snap shot of the sensor when an object is detected
+        if (myImager.isDataReady() == true)
+        {
+          if (myImager.getRangingData(&measurementData)) //Read distance data into array
+          {
+            //The ST library returns the data transposed from zone mapping shown in datasheet
+            //Pretty-print data with increasing y, decreasing x to reflect reality
+            for (int y = 0 ; y <= imageWidth * (imageWidth - 1) ; y += imageWidth)
+            {
+        
+              for (int x = imageWidth - 1 ; x >= 0 ; x--)
+              {
+                Serial.print("\t");
+                Serial.print(measurementData.distance_mm[x + y]);
+              }
+              Serial.println();
+            }
+            Serial.println();
+          }
+        }
+
       } else {return 0;} //quits out the function if no object is detected
     }
 
@@ -129,13 +199,44 @@ int objectDetection(int* smol, bool* detection) {
       if (ave <= minDistance)
       {
         *smol = 1;
+        Serial.println("Smol= ");
+        Serial.print(*smol);
       }
     }else {return 0;}
   }
   return 0;
 } 
 
+int getDistance() {
+
+  //Variables
+  int array[4] = {(35,36,27,28)}; //Sensor indexes for center 4 regions
+  int x = 0;
+
+  for (int i = 0; i <= 3; ++i)
+    {
+      x = x + measurementData.distance_mm[array[i]];
+    }
+
+  x = x/4; //Gets the average
+
+  Serial.print("Distance =");
+  Serial.println(x);
+
+  return x;
+}
+
+
+
 int heightCalculator(int smol) {
+/*
+|
+|
+Calculates the height of a detected object and outputs the heights
+|
+|
+*/
+ 
   //All of the variable nesscary for height calculations
   //All values in mm
   //Distance Variables
@@ -144,7 +245,7 @@ int heightCalculator(int smol) {
   int distance3 = 0;
 
   //Height Variables
-  int hieght2 = 0;
+  int height2 = 0;
   int height3 = 0;
   int heightTotal = 0;
 
@@ -152,20 +253,172 @@ int heightCalculator(int smol) {
   int theta1 = 0;
   int theta2 = 0;
   
+  //Measures the height of an object taller than the wrover.-----------------------------------------------
   if (smol == 1)
-  {
-      //Measures D1
-      int array[4] = {(35,36,27,28)}; 
-      
-      //Sums the values for the distances read by the center four zones on the sensor.
-      for (int i = 0; i <= 3; ++i)
+  { 
+    Serial.println("Taller");   
+    //Gets distance for D1
+    distance1 = getDistance(); 
+
+    Serial.print("Distance1= ");
+    Serial.println(distance1);
+    delay(500);
+
+    //Calculates theta 1
+    theta1 = atan(height1/distance1);
+    Serial.print("theta1= ");
+    Serial.println(theta1);
+    delay(500); 
+
+    //Moves the pitch Servo to theta one of the x-axis
+    pitchServo.write(90 - theta1); 
+
+    distance2 = getDistance();
+    Serial.print("distance2= ");
+    Serial.println(distance2);
+    delay(500); 
+
+
+    pitchServo.write(90); //Move servo back to neutral
+
+    //Variables needed to find top of object
+    int current = 0;
+    int previous = distance1;
+
+    //Finds the top of the object
+    for(int i = 0; i <= 90; ++i)
       {
-        distance1 = distance1 + measurementData.distance_mm[array[i]];
+//Snapshot of sensor values
+if (myImager.isDataReady() == true)
+  {
+    if (myImager.getRangingData(&measurementData)) //Read distance data into array
+    {
+      //The ST library returns the data transposed from zone mapping shown in datasheet
+      //Pretty-print data with increasing y, decreasing x to reflect reality
+      for (int y = 0 ; y <= imageWidth * (imageWidth - 1) ; y += imageWidth)
+      {
+        
+        for (int x = imageWidth - 1 ; x >= 0 ; x--)
+        {
+          Serial.print("\t");
+          Serial.print(measurementData.distance_mm[x + y]);
+        }
+        Serial.println();
+      }
+      Serial.println();
+    }
+  }
+    
+        delay(250);
+        int current = getDistance();
+
+        //Checks to see if a change in distance was more than 10 cm
+        if ((current-previous) < 100)
+        {
+          previous = current;
+        }
+        else 
+        {
+          //Found the top so read previous values
+          theta2 = pitchServo.read()-1;
+          distance3 = previous;
+          Serial.print("distance3= ");
+          Serial.println(distance3);
+          delay(500); 
+
+          
+          break;
+        }
+
+        //Moves servo to next increment
+        Serial.println("Increment Servo....");
+        delay(1000);
+        pitchServo.write(i);
+        Serial.println(i);
       }
 
-      theta1 = atan (height1/distance1); //Calculates theta 1
+    height3 = distance3*sin(theta2);
+    Serial.print("Height3= ");
+    Serial.println(height3);
+    delay(500); 
 
-      
+    
+    heightTotal = height2 + height3;
+    Serial.print("heightTotal= ");
+    Serial.println(heightTotal);
+    delay(500); 
 
+  } //End of taller object height calculation.
+  else //Find height of shorter object
+  {
+Serial.println("Shorter");
+
+    pitchServo.write(45); //Move servo back to neutral
+
+    //Variables needed to find top of object
+    int current = 0;
+    int previous = getDistance();
+    
+    //Finds the top of the object
+    for(int i = 90; i <= 135; ++i)
+    {
+//Snapshot of sensor values
+if (myImager.isDataReady() == true)
+  {
+    if (myImager.getRangingData(&measurementData)) //Read distance data into array
+    {
+      //The ST library returns the data transposed from zone mapping shown in datasheet
+      //Pretty-print data with increasing y, decreasing x to reflect reality
+      for (int y = 0 ; y <= imageWidth * (imageWidth - 1) ; y += imageWidth)
+      {
+        
+        for (int x = imageWidth - 1 ; x >= 0 ; x--)
+        {
+          Serial.print("\t");
+          Serial.print(measurementData.distance_mm[x + y]);
+        }
+        Serial.println();
+      }
+      Serial.println();
+    }
   }
+      delay(250);
+      int current = getDistance();
+
+      //Checks to see if a change in distance was more than 10 cm
+      if ((current-previous) < 100)
+      {
+        previous = current;
+      }
+      else 
+      {
+        //Found the top so read previous values
+        theta1 = pitchServo.read()-1;
+        distance1 = previous;
+        break;
+      }
+
+      //Moves servo to next increment
+      Serial.println("Increment Servo....");
+      delay(1000);
+      pitchServo.write(i);
+      Serial.println(i);
+    }
+
+    distance2 = distance1*cos(theta1);
+
+    theta2 = atan(height1/distance2);
+
+    pitchServo.write(90-theta2);
+
+    distance3 = getDistance();
+
+    height2 = distance2*tan(theta2);
+
+    height3 = distance1*sin(theta1);
+
+    heightTotal = height2-height3;
+  }
+
+  return heightTotal;
 }
