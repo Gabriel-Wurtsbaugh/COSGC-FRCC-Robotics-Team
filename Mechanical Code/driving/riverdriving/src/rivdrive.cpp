4f68975a5409741245cdde9d390618b1e8c9124c
River Bartell <rivbartell@gmail.com>
2024-03-10 14:20:22 -0600

    my functions were completely broken dont mind me

--- Mechanical Code/driving/riverdriving/src/rivdrive.cpp
+++ Mechanical Code/driving/riverdriving/src/rivdrive.cpp
@@ -2,11 +2,11 @@
 
 //these make it so visual studio code doesn't give me 5 billion errors 
 #include <Arduino.h>
-void Forward(int Duration);
-void Reverse(int Duration);
+void Forward(int Duration, int pwm_A, int pwm_B);
+void Reverse(int Duration, int pwm_A, int pwm_B);
 void Stop(int Duration);
-void RotateRight(int Duration);
-void RotateLeft(int Duration);
+void RotateRight(int Duration, int pwm_A, int pwm_B);
+void RotateLeft(int Duration, int pwm_A, int pwm_B);
 
 //Side A (right)
 int ENA_PIN = 27; //speed
@@ -17,6 +17,10 @@ int IN2_PIN = 14; //direction
 int ENB_PIN = 35; //speed
 int IN3_PIN = 33; //direction
 int IN4_PIN = 32; //direction
+
+//time of flight
+int SDA_PIN = 21;
+int SCL_PIN = 22;
 
 //make it go straight
 int straight_A = 255;
@@ -37,19 +41,19 @@ Serial.begin(115200);
 //loop for testing
 void loop () {
 Serial.println("Vroom vroom");
-Forward(4000);
+Forward(4000, straight_A, straight_B);
 Stop(1300);
-Reverse(4000);
+Reverse(4000, straight_A, straight_B);
 Stop(1300);
-RotateLeft(4000);
+RotateLeft(4000, straight_A, straight_B);
 Stop(1300);
-RotateRight(4000);
+RotateRight(4000, straight_A, straight_B);
 Serial.println("Hopefully I did the thing");
 Stop(5000);
 
 }
 
-//copy pasted this bad boy
+/* copy pasted this bad boy
 void loop () {
   IRsensorValue = analogRead(IRsensorPin);
   IRsensorVoltage = 5.0*(IRsensorValue/1023.);
@@ -71,9 +75,9 @@ else
 
 }
 }
+*/ 
 
-
-void Forward(int Duration)
+void Forward(int Duration, int pwm_A, int pwm_B)
 {
 //Function to drive forward for amount of time Duration
 
@@ -87,13 +91,13 @@ digitalWrite(IN2_PIN, LOW);
 digitalWrite(IN3_PIN, HIGH);
 digitalWrite(IN4_PIN, LOW);
 //pwm yippee
-analogWrite(ENA_PIN, straight_A);
-analogWrite(ENB_PIN, straight_B);
+analogWrite(ENA_PIN, pwm_A);
+analogWrite(ENB_PIN, pwm_B);
 delay(Duration);
 
 }
 
-void Reverse(int Duration)
+void Reverse(int Duration, int pwm_A, int pwm_B)
 {
 //Function to drive reverse for amount of time Duration
 
@@ -107,8 +111,8 @@ digitalWrite(IN2_PIN, HIGH);
 digitalWrite(IN3_PIN, LOW);
 digitalWrite(IN4_PIN, HIGH);
 //pwm yippee
-analogWrite(ENA_PIN, straight_A);
-analogWrite(ENB_PIN, straight_B);
+analogWrite(ENA_PIN, pwm_A);
+analogWrite(ENB_PIN, pwm_B);
 delay(Duration);
 
 }
@@ -125,7 +129,7 @@ delay(Duration);
 
 }
 
-void RotateRight(int Duration)
+void RotateRight(int Duration, int pwm_A, int pwm_B)
 {
 //Function to drive right rotation for amount of time Duration
 
@@ -139,13 +143,13 @@ digitalWrite(IN2_PIN, HIGH);
 digitalWrite(IN3_PIN, HIGH);
 digitalWrite(IN4_PIN, LOW);
 //pwm yippee
-analogWrite(ENA_PIN, straight_A);
-analogWrite(ENB_PIN, straight_B);
+analogWrite(ENA_PIN, pwm_A);
+analogWrite(ENB_PIN, pwm_B);
 delay(Duration);
 
 }
 
-void RotateLeft(int Duration)
+void RotateLeft(int Duration, int pwm_A, int pwm_B)
 {
 //Function to drive left rotation for amount of time Duration
 
@@ -159,8 +163,8 @@ digitalWrite(IN2_PIN, LOW);
 digitalWrite(IN3_PIN, LOW);
 digitalWrite(IN4_PIN, HIGH);
 //pwm yippee
-analogWrite(ENA_PIN, straight_A);
-analogWrite(ENB_PIN, straight_B);
+analogWrite(ENA_PIN, pwm_A);
+analogWrite(ENB_PIN, pwm_B);
 delay(Duration);
 
 }
