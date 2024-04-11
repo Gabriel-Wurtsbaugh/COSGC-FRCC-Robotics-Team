b72437ffb31cc152b2062f57a6f03617089c19b9
Gabe Labtop <gwurtsy@gmail.com>
2024-04-11 00:05:15 -0600

    Fixed stupid formatting

--- Sensor Code/Object Recognition/src/main.cpp
+++ Sensor Code/Object Recognition/src/main.cpp
@@ -141,46 +141,45 @@ bool dataProcessed() {
 bool objectDetection()
 {
   for (int y = 0 ; y <= imageWidth * (imageWidth - 1) ; y += imageWidth)
+  {
+    for (int x = imageWidth - 1 ; x >= 0 ; x--)
+    {
+      //Rejects the values from all of the corner zones when doing general object detection
+      switch (x+y)
       {
-        
-        for (int x = imageWidth - 1 ; x >= 0 ; x--)
+      case 56:
+      break;
+      case 57:
+      break;
+      case 48:
+      break;
+      case 62:
+      break;
+      case 63:
+      break;
+      case 55:
+      break;
+      case 15:
+      break;
+      case 7:
+      break;
+      case 6:
+      break;
+      case 8:
+      break;
+      case 0:
+      break;
+      case 1:
+      break;
+      default:
+        if (measurementData.distance_mm[x+y] < 300)
         {
-            //Rejects the values from all of the corner zones when doing general object detection
-            switch (x+y)
-            {
-            case 56:
-              break;
-            case 57:
-              break;
-            case 48:
-              break;
-            case 62:
-              break;
-            case 63:
-              break;
-            case 55:
-              break;
-            case 15:
-              break;
-            case 7:
-              break;
-            case 6:
-              break;
-            case 8:
-              break;
-            case 0:
-              break;
-            case 1:
-              break;
-            default:
-              if (measurementData.distance_mm[x+y] < 300)
-                {
-                  return true;
-                }         
-              break;
-            }
-        }
+        return true;
+        }         
+        break;
       }
+    }
+  }
 }
 
 //Checks to see if an object has come within the min detect distance
